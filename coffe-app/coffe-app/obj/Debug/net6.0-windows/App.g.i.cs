d4ce41a5ccda0e6c8d3ab236333063d20c528c6b
Vicktorya-s <svirzhevskayavika@mail.ru>
2024-11-15 15:24:41 +0300

    Главное окно и профиль пользователя

--- coffe-app/coffe-app/obj/Debug/net6.0-windows/App.g.i.cs
+++ coffe-app/coffe-app/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace coffe_app {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace coffe_app {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             coffe_app.App app = new coffe_app.App();
             app.InitializeComponent();
